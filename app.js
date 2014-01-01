dd259a3744598624b3b216cede6c220355d31ec8
Ramon Liu <llmfei@gmail.com>
2013-12-31 19:07:41 -0800

    reformat code

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -9,7 +8,7 @@ var path = require('path');
 
 var app = express(),
   server = require('http').createServer(app)
-  io = require('socket.io').listen(server);
+io = require('socket.io').listen(server);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -36,25 +35,23 @@ app.get('/', function (req, res) {
 
 var board =
 {
-  columns: 3,
-  rows: 3,
-  state: {
-    u: 0,
-    o: 1,
-    x: -1
+  columns:3,
+  rows:3,
+  state:{
+    u:0,
+    o:1,
+    x:-1
   },
-  cells: [],
-  players: [],
+  cells:[],
+  players:[],
   nextplayer:null,
-gameOver:null
+  gameOver:null
 };
 
 (function initBoard() {
-  for (var i = 0; i < board.rows; i++)
-  {
+  for (var i = 0; i < board.rows; i++) {
     board.cells[i] = [];
-    for (var j = 0; j < board.columns; j++)
-    {
+    for (var j = 0; j < board.columns; j++) {
       board.cells[i][j] = board.state.u;
     }
   }
@@ -63,124 +60,120 @@ gameOver:null
 io.sockets.on('connection', function (socket) {
   socket.on('login', function (data) {
     console.log(data.player);
-            if(board.nextplayer === null)
-            {
-            board.nextplayer=data.player;
-            }
+    if (board.nextplayer === null) {
+      board.nextplayer = data.player;
+    }
     board.players.push(data.player);
     socket.join('players').emit('login', {
-      status: 'ok'
-    }).emit('board', {board: board});
+      status:'ok'
+    }).emit('board', {board:board});
   });
 
-  socket.on('cell', function(data){
+  socket.on('cell', function (data) {
     console.log("=====cell event=====");
     console.log(data);
-            if(data.state == "o")
-            {
-            board.nextplayer="x";
-            } else
-            {
-            board.nextplayer="o";
-            }
+    if (data.state == "o") {
+      board.nextplayer = "x";
+    } else {
+      board.nextplayer = "o";
+    }
 
     board.cells[data.row][data.column] = data.state;
-            
-            // TODO: check game over or not
-            // if game over, set flag in data, data.gameOver = x/o/xo
-            console.log("CC Test");
-            
-            var winner = null;
-            var draw ,row0, row1, row2, c0, c1, c2, d1, d2;
-            row0 = row1 = row2 = c0 = c1 = c2 = d1 = d2 = draw = 0;
-            
-            //count the number of spaces filled on the board
-            for ( var r = 0; r < 3; r++)
-            {
-            for ( var c = 0; c < 3; c++)
-            {
-            if(board.cells[r][c] == "x"){draw = draw + 1} else if (board.cells[r][c] == "o") { draw = draw + 1} {draw = draw}
-            
-            }
-            }
-            
-            //create a numberic board
-            var numboard = [[0,0,0],[0,0,0],[0,0,0]];
 
-            for ( var r = 0; r < 3; r++)
-            {
-            for ( var c = 0; c < 3; c++)
-            {
-            if(board.cells[r][c] == "x")
-            {
-            numboard[r][c] = -1;
-            } else if (board.cells[r][c] == "o")
-            {
-            numboard[r][c] = 1;
-            }
-            }
-            }
-            
-            //check if anyone has won on the x and y axis
-            for ( var r = 0; r < 3; r++)
-            {
-            c0 = numboard[r][0] + c0;
-            c1 = numboard[r][1] + c1;
-            c2 = numboard[r][2] + c2;
-            }
-            
-            for ( var c = 0; c < 3; c++)
-            {
-            row0 = numboard[0][c] + row0;
-            row1 = numboard[1][c] + row1;
-            row2 = numboard[2][c] + row2;
-            }
-            
-            //check if anyone has won on the diagonals
-            d1 = (numboard[0][0]+numboard[1][1]+numboard[2][2]);
-            d2 = (numboard[2][0]+numboard[1][1]+numboard[0][2]);
-            
-            //see who has won finally
-            if (draw >=5)
-            {
-            var cc = -3;
-                if((row0 == cc)||(row1 == cc)||(row2 == cc)|| (c0 == cc) || (c1 == cc) || (c2 == cc) || (d1 == cc) || (d2 == cc))
-            {
-                winner = "X";
-            }
-            var cc = 3;
-            if((row0 == cc)||(row1 == cc)||(row2 == cc)|| (c0 == cc) || (c1 == cc) || (c2 == cc) || (d1 == cc) || (d2 == cc))
-            {
-            winner = "O";
-            }
-            }
-            
-            // if board is full and winner is null then its a draw
-            if((draw == 9) && (winner == null))
-            {
-            winner = "draw";
-            }
-            
-            board.gameOver = winner;
-            
-            //test logs
-            //console.log("draw is "+draw);
-            //console.log("numboard is"+numboard[0][0]);
-            //console.log("top left cell" + board.cells[0][0]);
-            console.log("row0, row1, row2, c0, c1, c2, d1, d2");
-            console.log(row0, row1, row2, c0, c1, c2, d1, d2);
-            //console.log("row 0 is" + row0);
-            console.log("winner is " + winner);
-            
-            // send over status
-            data.gameOver = board.gameOver;
-            
-            //
-            data.nextplayer = board.nextplayer;
+    // TODO: check game over or not
+    // if game over, set flag in data, data.gameOver = x/o/xo
+    console.log("CC Test");
+
+    var winner = null;
+    var draw , row0, row1, row2, c0, c1, c2, d1, d2;
+    row0 = row1 = row2 = c0 = c1 = c2 = d1 = d2 = draw = 0;
+
+    //count the number of spaces filled on the board
+    for (var r = 0; r < 3; r++) {
+      for (var c = 0; c < 3; c++) {
+        if (board.cells[r][c] == "x") {
+          draw = draw + 1
+        } else if (board.cells[r][c] == "o") {
+          draw = draw + 1
+        }
+        {
+          draw = draw
+        }
+
+      }
+    }
+
+    //create a numberic board
+    var numboard = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ];
+
+    for (var r = 0; r < 3; r++) {
+      for (var c = 0; c < 3; c++) {
+        if (board.cells[r][c] == "x") {
+          numboard[r][c] = -1;
+        } else if (board.cells[r][c] == "o") {
+          numboard[r][c] = 1;
+        }
+      }
+    }
+
+    //check if anyone has won on the x and y axis
+    for (var r = 0; r < 3; r++) {
+      c0 = numboard[r][0] + c0;
+      c1 = numboard[r][1] + c1;
+      c2 = numboard[r][2] + c2;
+    }
+
+    for (var c = 0; c < 3; c++) {
+      row0 = numboard[0][c] + row0;
+      row1 = numboard[1][c] + row1;
+      row2 = numboard[2][c] + row2;
+    }
+
+    //check if anyone has won on the diagonals
+    d1 = (numboard[0][0] + numboard[1][1] + numboard[2][2]);
+    d2 = (numboard[2][0] + numboard[1][1] + numboard[0][2]);
+
+    //see who has won finally
+    if (draw >= 5) {
+      var cc = -3;
+      if ((row0 == cc) || (row1 == cc) || (row2 == cc) || (c0 == cc) || (c1 == cc) || (c2 == cc) || (d1 == cc) || (d2 == cc)) {
+        winner = "X";
+      }
+      var cc = 3;
+      if ((row0 == cc) || (row1 == cc) || (row2 == cc) || (c0 == cc) || (c1 == cc) || (c2 == cc) || (d1 == cc) || (d2 == cc)) {
+        winner = "O";
+      }
+    }
+
+    // if board is full and winner is null then its a draw
+    if ((draw == 9) && (winner == null)) {
+      winner = "draw";
+    }
+
+    board.gameOver = winner;
+
+    //test logs
+    //console.log("draw is "+draw);
+    //console.log("numboard is"+numboard[0][0]);
+    //console.log("top left cell" + board.cells[0][0]);
+    console.log("row0, row1, row2, c0, c1, c2, d1, d2");
+    console.log(row0, row1, row2, c0, c1, c2, d1, d2);
+    //console.log("row 0 is" + row0);
+    console.log("winner is " + winner);
+
+    // send over status
+    data.gameOver = board.gameOver;
+
+    //
+    data.nextplayer = board.nextplayer;
     io.sockets.in('players').emit("cell", data);
   });
 });
 
-server.listen(app.get('port'), function(){
+server.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,9 +3,8 @@
  * Date: 12/27/13 8:51 AM
  */
 
-$(function()
-{
-  $.isNumeric = function(str) {
+$(function () {
+  $.isNumeric = function (str) {
     return str && !isNaN(parseFloat(str));
   }
 
@@ -13,23 +12,22 @@ $(function()
 
   var board =
   {
-    columns: 3,
-    rows: 3,
-    state: {
-      u: 0,
-      o: 1,
-      x: -1
+    columns:3,
+    rows:3,
+    state:{
+      u:0,
+      o:1,
+      x:-1
     },
-    player: null,
-  nextplayer: null,
-    cells: [],
-  gameOver:null
+    player:null,
+    nextplayer:null,
+    cells:[],
+    gameOver:null
   };
 
   var socket = null;
 
-  function init()
-  {
+  function init() {
     initListeners();
     initSocket();
   }
@@ -38,9 +36,8 @@ $(function()
    ****************************** UI *******************************
    *****************************************************************/
 
-  function initListeners()
-  {
-    $('.player').click(function(event){
+  function initListeners() {
+    $('.player').click(function (event) {
       var srcId = event.srcElement.id;
       board.player = srcId.toLowerCase().replace('player', '');
       $('#playerName').text($(event.srcElement).text()).addClass('player' + board.player.toUpperCase());
@@ -50,18 +47,15 @@ $(function()
     });
   }
 
-  function initPlayBoard()
-  {
+  function initPlayBoard() {
     var $board = $("#playBoard");
-    for (var i = 0; i < board.rows; i++)
-    {
-      for (var j = 0; j < board.columns; j++)
-      {
+    for (var i = 0; i < board.rows; i++) {
+      for (var j = 0; j < board.columns; j++) {
         $board.append(createCell(i, j, board.cells[i][j]));
       }
     }
 
-    $board.click(function(event){
+    $board.click(function (event) {
       if ($(event.srcElement).hasClass('cell')) {
         var cellId = event.srcElement.id;
         var ps = getCellPositionFromId(cellId);
@@ -92,13 +86,14 @@ $(function()
 
   function getCellPositionFromId(cellId) {
     var ps = cellId.replace("cell", "").split("-");
-    return ps.map(function(i){return parseInt(i)});
+    return ps.map(function (i) {
+      return parseInt(i)
+    });
   }
-  
+
   function clickCell(row, column) {
     console.log("click " + row + ", " + column);
-    if ((board.cells[row][column] == board.state.u) && (board.nextplayer==board.player))
-    {
+    if ((board.cells[row][column] == board.state.u) && (board.nextplayer == board.player)) {
       setCellState(row, column, board.player);
       sendCellState(row, column, board.player);
     }
@@ -113,18 +108,17 @@ $(function()
     $('#' + getCellId(row, column)).addClass(getStateClass(state));
     console.log(board);
   }
-  
+
   function displaygameOver(result) {
     var textResult = result;
-  // TODO: get result to display, lose or win or tie
-  if(textResult == "draw")
-  {
-  more = "";
-  } else {
-  more = " WINS!!!! ";
-  }
-  
-  var oldText = $('#playerName').text();
+    // TODO: get result to display, lose or win or tie
+    if (textResult == "draw") {
+      more = "";
+    } else {
+      more = " WINS!!!! ";
+    }
+
+    var oldText = $('#playerName').text();
     $('#playerName').text(oldText + ': ' + textResult + more);
   }
 
@@ -132,49 +126,46 @@ $(function()
   /*****************************************************************
    ************************ Talk with Server ***********************
    *****************************************************************/
-  function initSocket()
-  {
+  function initSocket() {
     socket = io.connect();
 
-    socket.on('board', function (data)
-    {
+    socket.on('board', function (data) {
       var serverBoard = data.board;
       console.log(serverBoard);
       $.extend(board, serverBoard);
       initPlayBoard();
     });
 
-    socket.on('login', function(data){
+    socket.on('login', function (data) {
       console.log("========login=====");
       console.log(data);
       isLogin = true;
     });
 
-    socket.on('cell', function(cell){
+    socket.on('cell', function (cell) {
       setCellState(cell.row, cell.column, cell.state);
-              board.nextplayer = cell.nextplayer;
-            
-              // TODO: check game over
-              board.gameOver = cell.gameOver;
-              if((board.gameOver !== null)||(board.gameOver == "draw"))
-              {
-              displaygameOver(board.gameOver);
-              }
+      board.nextplayer = cell.nextplayer;
+
+      // TODO: check game over
+      board.gameOver = cell.gameOver;
+      if ((board.gameOver !== null) || (board.gameOver == "draw")) {
+        displaygameOver(board.gameOver);
+      }
     });
   }
 
   function sendLoginRequest(player) {
     socket.emit('login', {
-      player: player
+      player:player
     });
   }
 
   function sendCellState(row, column, state) {
     socket.emit('cell', {
-      row: row,
-      column: column,
-      state: state
-        //state is the player
+      row:row,
+      column:column,
+      state:state
+      //state is the player
     });
   }
 
